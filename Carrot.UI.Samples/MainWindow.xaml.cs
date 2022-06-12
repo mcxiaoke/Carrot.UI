694fabcec4dc301a2ff627266cb5db600ee2b09c
mcxiaoke <mcxiaoke@live.com>
2022-06-12 17:37:08 +0800

    carrot ui color combobox v0.2.0

--- Carrot.UI.Samples/MainWindow.xaml.cs
+++ Carrot.UI.Samples/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Carrot.UI.Controls;
+using Carrot.UI.Controls.Utils;
 
 namespace Carrot.UI.Samples {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window {
+        private List<KeyValuePair<string, Color>> _extraColors = new List<KeyValuePair<string, Color>>() {
+            new KeyValuePair<string, Color>("TestColor1",
+                UIHelper.ParseColor("#123456")),
+            new KeyValuePair<string, Color>("TestColor2",
+                UIHelper.ParseColor("#80cccccc")),
+        };
+
+        public List<KeyValuePair<string, Color>> CustomColors {
+            get => _extraColors;
+            set => _extraColors = value;
+        }
+
+
         public MainWindow() {
             InitializeComponent();
+            colorBox.DataContext = CustomColors;
+        }
+
+        private void ColorBox_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<KeyValuePair<string, Color>> e) {
+            Debug.WriteLine($"ColorBox_SelectedColorChanged changed {e.OldValue} => {e.NewValue}");
         }
     }
 }
--- Carrot.UI/Picker/ColorComboBox.xaml.cs
+++ Carrot.UI/Picker/ColorComboBox.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
+using Carrot.UI.Controls;
+using Carrot.UI.Controls.Utils;
+using Color = System.Windows.Media.Color;
 
 namespace Carrot.UI.Controls.Picker {
     /// <summary>
@@ -20,33 +22,44 @@ namespace Carrot.UI.Controls.Picker {
     /// </summary>
     public partial class ColorComboBox : UserControl {
 
-        public static T Clamp<T>(T value, T min, T max)
-where T : System.IComparable<T> {
-            T result = value;
-            if (value.CompareTo(max) > 0)
-                result = max;
-            if (value.CompareTo(min) < 0)
-                result = min;
-            return result;
-        }
+        public static KeyValuePair<string, Color> InvalidColorItem = new KeyValuePair<string, Color>("Invalid", UIHelper.ParseColor("#000001"));
 
         private static IEnumerable<KeyValuePair<string, Color>> GetAllColors() =>
             typeof(Colors).GetProperties()
             .Where(prop => typeof(Color).IsAssignableFrom(prop.PropertyType))
             .Select(prop => new KeyValuePair<string, Color>(prop.Name, (Color)prop.GetValue(null)));
 
+        #region DependencyProperty
+
+        public static readonly DependencyProperty ExtraColorsProperty =
+    DependencyProperty.Register(nameof(ExtraColors), typeof(List<KeyValuePair<string, Color>>),
+        typeof(ColorComboBox), new PropertyMetadata(null));
 
         public static readonly DependencyProperty ColumnCountProperty =
-  DependencyProperty.Register(nameof(ColumnCount), typeof(int),
-    typeof(ColorComboBox), new PropertyMetadata(1));
+            DependencyProperty.Register(nameof(ColumnCount), typeof(int),
+                typeof(ColorComboBox), new PropertyMetadata(1));
 
         public static readonly DependencyProperty SelectedIndexProperty =
-DependencyProperty.Register(nameof(SelectedIndex), typeof(int),
-typeof(ComboBox), new PropertyMetadata(-1));
+            DependencyProperty.Register(nameof(SelectedIndex), typeof(int),
+                typeof(ComboBox), new PropertyMetadata(-1));
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(nameof(SelectedItem), typeof(object),
+                typeof(ComboBox), new PropertyMetadata(null));
+
+        #endregion
+
+        public List<KeyValuePair<string, Color>> ExtraColors {
+            get => (List<KeyValuePair<string, Color>>)GetValue(ExtraColorsProperty) ?? new List<KeyValuePair<string, Color>>();
+            set {
+                SetValue(ExtraColorsProperty, value);
+                //cmbColors.Items.Insert(0, value);
+            }
+        }
 
         public int ColumnCount {
-            get => Clamp((int)GetValue(ColumnCountProperty), 1, 6);
-            set => SetValue(ColumnCountProperty, Clamp(value, 1, 6));
+            get => Utilities.Clamp((int)GetValue(ColumnCountProperty), 1, 6);
+            set => SetValue(ColumnCountProperty, Utilities.Clamp(value, 1, 6));
         }
 
         public int SelectedIndex {
@@ -54,13 +67,28 @@ typeof(ComboBox), new PropertyMetadata(-1));
             set => cmbColors.SelectedIndex = value;
         }
 
+        public object SelectedItem {
+            get => cmbColors.SelectedItem;
+            set => cmbColors.SelectedItem = value;
+        }
 
         public ColorComboBox() {
             InitializeComponent();
-            cmbColors.ItemsSource = GetAllColors();
+        }
+
+        private void ColorComboBox_Loaded(object sender, RoutedEventArgs e) {
+            Debug.WriteLine("ColorComboBox_Loaded");
+            foreach (KeyValuePair<string, Color> item in ExtraColors) {
+                Debug.WriteLine($"loaded extra {item.Key} {item.Value}");
+            }
+            var allColors = new List<KeyValuePair<string, Color>>();
+            allColors.AddRange(ExtraColors);
+            allColors.AddRange(GetAllColors());
+            cmbColors.ItemsSource = allColors;
         }
 
         private void ComboBox_Table_Loaded(object sender, RoutedEventArgs e) {
+            Debug.WriteLine("ComboBox_Table_Loaded");
             if (sender is Grid grid) {
                 if (grid.RowDefinitions.Count == 0) {
                     for (int r = 0; r <= cmbColors.Items.Count / ColumnCount; r++) {
@@ -79,6 +107,47 @@ typeof(ComboBox), new PropertyMetadata(-1));
             }
         }
 
+        #region Event Handlers
+        private void CmbColors_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            KeyValuePair<string, Color> oldColor = InvalidColorItem;
+            KeyValuePair<string, Color> newColor = InvalidColorItem;
+            if (e.RemovedItems.Count > 0) {
+                oldColor = (KeyValuePair<string, Color>)e.RemovedItems[0];
+            }
+            if (e.AddedItems.Count > 0) {
+                newColor = (KeyValuePair<string, Color>)e.AddedItems[0];
+            }
+            Debug.WriteLine($"SelectionChanged old={oldColor} new={newColor}");
+            var newEventArgs = new RoutedPropertyChangedEventArgs<KeyValuePair<string, Color>>
+                (oldColor, newColor) { RoutedEvent = SelectedColorChangedEvent };
+
+            RaiseEvent(newEventArgs);
+        }
+
+
+
+        public static readonly RoutedEvent SelectedColorChangedEvent =
+    EventManager.RegisterRoutedEvent(nameof(SelectedColorChanged),
+        RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<KeyValuePair<string, Color>>),
+        typeof(ColorComboBox));
+
+
+        public event RoutedPropertyChangedEventHandler<KeyValuePair<string, Color>> SelectedColorChanged {
+            add {
+                AddHandler(SelectedColorChangedEvent, value);
+            }
+
+            remove {
+                RemoveHandler(SelectedColorChangedEvent, value);
+            }
+        }
+
+
+        private void CmbColors_DropDownClosed(object sender, EventArgs e) {
+            Debug.WriteLine($"CmbColors_DropDownClosed selected={cmbColors.SelectedItem}");
+        }
+
+        #endregion
     }
 
 
